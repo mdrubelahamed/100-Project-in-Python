9e163fb5664b54ae909b085feb1829cb49f1adef
mdrubelahamed1 <141414730+mdrubelahamed1@users.noreply.github.com>
2023-08-13 00:58:44 +0530

    Update

--- day3/main.py
+++ day3/main.py
@@ -2,20 +2,27 @@
 # Treasure Island game
 
 
-#rollercoaster ride
-# print("Welcome to the roller coaster!")
-# height = int(input("What is your height in cm? "))
+# rollercoaster ride
+print("Welcome to the roller coaster!")
+height = int(input("What is your height in cm? "))
 
-# if height >= 120 :
-#     print("You can ride the roller coaster!")
-# else:
-#     print("Sorry, you have to grow taller before you can ride.")
-
-
-
-#checking odd even number
-number = int(input("Which number do you want to check? "))
-if number%2 == 0:
-    print(f"{number} is an even number.")
+if height >= 120 :
+    print("You can ride the roller coaster!")
+    age = int(input("what is your age? "))
+    if age < 12:
+        print("Your Ticket Price is $5")
+    elif 12 <= age <= 18:
+        print("Your Ticket Price is $7")
+    else:
+        print("Your Ticket Price is $12")
 else:
-    print(f"{number} is an odd number.")
+    print("Sorry, you have to grow taller before you can ride.")
+
+
+
+# #checking odd even number
+# number = int(input("Which number do you want to check? "))
+# if number%2 == 0:
+#     print(f"{number} is an even number.")
+# else:
+#     print(f"{number} is an odd number.")
