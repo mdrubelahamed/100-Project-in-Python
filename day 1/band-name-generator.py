a39cd297756910fd009b2727ce6b89b0e83532c8
mdrubelahamed1 <141414730+mdrubelahamed1@users.noreply.github.com>
2023-08-28 09:42:37 +0530

    modified
    modified some files

--- day 1/band-name-generator.py
+++ day 1/band-name-generator.py
@@ -1,5 +1,1 @@
-print("welcome to the Band Name Generator.")
 cityname = input("What's the name of the city you grew up in?\n")
-nickname = input("what's your nickname?\n")
-
-print("your brand name could be",cityname + " "+ nickname)
--- day 9/dictionary.py
+++ day 9/dictionary.py
@@ -1,8 +1,3 @@
-programming_dict = {
-    "bug" : "An errro in a program that prevent the program running as expected",
-    "Function" : "A piece of code that you can easily call over and over again",
-    "language" : "A way to tell computer what we want to do"
-}
 
 # retrieve data from dictionary 
 # x = programming_dict["language"]
@@ -24,7 +19,13 @@ empty_dict = {}
 # programming_dict["language"] = "A way of communicating with Computer"
 # print(programming_dict)
 
-# loop in dictionary
+
+programming_dict = {
+    "bug" : "An errro in a program that prevent the program running as expected",
+    "Function" : "A piece of code that you can easily call over and over again",
+    "language" : "A way to tell computer what we want to do"
+}
+
 for thing in programming_dict:
     print(thing)
     # print(programming_dict[thing])
