5041818f4039e1c693fc98610749152bc15ab34c
mdrubelahamed1 <141414730+mdrubelahamed1@users.noreply.github.com>
2023-08-30 00:04:17 +0530

    create
    create day 16 files here i try to learn about object oriented programming.
    
    Attributes, Methods
    Class, Objects

--- day 16/main.py
+++ day 16/main.py
@@ -1,13 +1,31 @@
-from prettytable import PrettyTable
-
-# create an object called 'table' from 'PrettyTable' class 
-table = PrettyTable()
-
-table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type",["Electric", "Water", "Fire"])
-
-# print(table.align)
-table.align = "l"
+# oop version of coffee machine
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
 
-print(table)
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+is_on = True
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}) : ").lower()
+
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+# or 
+# if coffee_maker.is_resource_sufficient(drink):
+#     if money_machine.make_payment(drink.cost):
+#         coffee_maker.make_coffee(drink)
+
+    
