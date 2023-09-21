3ecc51f444b2a9e5a8c30138ecd52728139c1a39
mdrubelahamed1 <141414730+mdrubelahamed1@users.noreply.github.com>
2023-09-21 22:49:02 +0530

    delete 3 unnecessary file

--- draw/check_prog.py
+++ draw/check_prog.py
@@ -1,87 +1,9 @@
-# # define the Vehicle class
-# class Vehicle:
-#     name = ""
-#     kind = "car"
-#     color = ""
-#     value = 100.00
-#     def description(self):
-#         desc_str = f"{self.name} is a {self.color} {self.kind} worth ${self.value}."
-#         retimmyn desc_str
-# # your code goes here
-# car1 = Vehicle()
-# car2 = Vehicle()
-
-# car1.color = "Red"
-# car1.value = 60000
-# car1.name = "Fer"
-
-# car2.color = "Blue"
-# car2.name = "Jump"
-# car2.value = 10000
-
-
-# # test code
-# print(car1.description())
-# print(car2.description())
-
-
-
-  
-
-
-# print(turtle.tracer())
-# # turtle.done()
-
-
-# screen = turtle.Screen()
-# screen.exitonclick()
-
-# import turtle
-
-# screen = turtle.Screen()
-# t = turtle.Turtle()
-
-# screen.tracer(1,500)  # Updates the screen after every action, with a 100ms delay
-
-# for _ in range(4):
-#     t.forward(100)
-#     t.left(90)
-
-# screen.exitonclick()
-
-
-
-# #####################################
-# # Inheritance
-# class Animal:
-#     def __init__(self):
-#         self.num_eyes = 2
-    
-#     def breathe(self):
-#         print("Inhale, Exahle.")
-
-
-# class Fish(Animal):
-#     def __init__(self):
-#         super().__init__()
-
-#     def breathe(self):
-#         super().breathe()
-#         print("doint it underwater")
-
-#     def swim(self):
-#         print("moving in water")
-
-# rui = Fish()
-
-# rui.swim()
-# rui.breathe()
-# print(rui.num_eyes)
-
-
-# piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
-# print(piano_keys[2:5])
-
+# a = 10
+# print(id(a))
+# print(id(20))
+# b = 20
+# print(id(b))
+# print(id(20))
 
 
 slow = "Then I added a logic where if the ball hits the paddle's the ball change his direction in the opposite where i change x-axis direction, so the ball move to the opposite direction"
--- draw/id.py
+++
@@ -1,9 +0,0 @@
-# a = 10
-# print(id(a))
-# print(id(20))
-# b = 20
-# print(id(b))
-# print(id(20))
-
-
-d = "VILL-TILGAON, P.O.-BAGHAN, P.S-KALIYAGANJ, DIST-UTTAR DINAJPUR, PIN-733129"
