a273a6ac82008f9676127c116c7d0c8b338f34f9
mdrubelahamed1 <141414730+mdrubelahamed1@users.noreply.github.com>
2023-09-21 22:46:28 +0530

    delete HELLO.py file

--- draw/hard_level_game.py
+++ draw/hard_level_game.py
@@ -5,6 +5,15 @@
 # startgame.play_game()
 
 
-slow = """- Then I added some logic by which if the ball goes or too far from the screnn either right side or left side The ball will reset his position to the home position and if **goes to the Opposite side of paddle** """
+slow = """# day 6
+## Functions, Code Blocks, While Loops, Code Block , Indentation
+
+### Reborg's world game 
+[Reborg's world](https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json)
+
+#### Create a square function game
+#### Hurdle 1 game 
+
+### While loop"""
 
 print(slow.title())
