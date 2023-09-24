c0accaba3fe6e7af7017f5f2240c0db10a709247
mdrubelahamed1 <141414730+mdrubelahamed1@users.noreply.github.com>
2023-09-25 00:51:58 +0530

    create folder file
    file which create folder and inside main.py and README.md

---
+++ folder_creation.py
@@ -0,0 +1,10 @@
+import os
+
+path = "./project 24"
+
+os.mkdir(path=path)
+
+with open("./project 24/main.py", mode="w") as f1, open("./project 24/README.md", mode="w") as f2:
+    f1.write("")
+    f2.write("")
+
