3815e86a80f904bf297a262c1cc4c5dc33408044
mdrubelahamed1 <141414730+mdrubelahamed1@users.noreply.github.com>
2023-09-24 23:50:30 +0530

    create so many letter through (with , open, "w") in python

--- project 23/README.md
+++ project 23/README.md
@@ -7,6 +7,24 @@
 #     print(contents)
 
 # with open("project 23\\new_file.txt", mode="w") as file:
-#     file.write("This is text file, which is not present in the folder so first it will create the file and the it will write the text which i am providing..")```
+#     file.write("This is text file, which is not present in the folder so first it will create the file and the it will write the text which i am providing..")
+```
+```
+# with open(file="project 23\hello.txt", mode="w") as file:
+#     file.write("I am writing on a file which is not present in the currrent folder")
 
-## mail merging
+```
+
+## Mail Merging
+
+---
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp  
+#Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp     
+#Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp    
+
+---
--- project 23/main.py
+++ project 23/main.py
@@ -1,11 +1,12 @@
-# with open(file="project 23\hello.txt", mode="w") as file:
-#     file.write("I am writing on a file which is not present in the currrent folder")
 
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+with open("project 23/Input/Letters/starting_letter.txt") as mail_head, open("project 23/Input/Names/invited_names.txt") as invited_names:
+    mail_contents = mail_head.read()
+    name_list = invited_names.readlines()
+
+    for name in name_list:
+        name = name.strip("\n")
+        final_mail = mail_contents.replace("[name]", name)
+
+        with open(f"project 23/Output/ReadyToSend/letter_for_{name}.txt", mode="w") as readyToSendMail:
+            readyToSendMail.write(final_mail)
+
