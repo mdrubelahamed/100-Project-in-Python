051158303ca7968b4a24c68ac235a62ad0624252
mdrubelahamed <141414730+mdrubelahamed@users.noreply.github.com>
2024-01-20 23:23:07 +0530

    update

--- my_bin/main.py
+++ my_bin/main.py
@@ -18,12 +18,40 @@
 
 # --------------------------- Pandas Things --------------------------- #
 # 
-import pandas
+
+# import pandas
 
 # data = pandas.read_csv("draw/weather_data.csv")
 # # print(data)
 # data_dict = data.to_dict(orient='records')
 # # print(data_dict)
 
-for _ in range(110):
-    print("-", end="")
+# for _ in range(110):
+#     print("-", end="")
+
+# --------------------------- Pandas Work --------------------------- #
+
+import pandas as pd
+
+# df = pd.read_csv("my_bin/basic_file.csv")
+
+
+# # # How many elements does the dataframe have ?
+# # print(df.info())
+
+# # How much old 'Alice' are ?
+# print(df[df.Name == "Alice"].Age)
+
+#-----------
+df = pd.read_csv("my_bin/nyc_weather.csv")
+
+# print(df)
+
+# find_ans = df.EST[df.Events == "Rain"] # Which date rain
+
+
+# average WindSpeedMPH
+df.fillna(0, inplace=True)
+find_ans = df.WindSpeedMPH.mean()
+
+print(find_ans)
